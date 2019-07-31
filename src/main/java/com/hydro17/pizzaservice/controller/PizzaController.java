6a7c4af1a440cedca0449cb7845fabea06e254e1
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-07-31 10:00:14 +0200

    add internal class 'PizzaDTO' and use it to pass to the view template:  'id', 'name', 'ingredients' (as string), 'price'

--- src/main/java/com/hydro17/pizzaservice/controller/PizzaController.java
+++ src/main/java/com/hydro17/pizzaservice/controller/PizzaController.java
@@ -1,9 +1,12 @@
 package com.hydro17.pizzaservice.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +15,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hydro17.pizzaservice.dao.IngredientDAO;
 import com.hydro17.pizzaservice.dao.PizzaDAO;
+import com.hydro17.pizzaservice.entity.Ingredient;
 import com.hydro17.pizzaservice.entity.Pizza;
 
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
+	
+	private class PizzaDTO {
+		private int id;
+		private String name;
+		private String ingredients;
+		private double price;
+		
+		public PizzaDTO(int id, String name, String ingredients, double price) {
+			this.id = id;
+			this.name = name;
+			this.ingredients = ingredients;
+			this.price = price;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getIngredients() {
+			return ingredients;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+	}
 	
 	@Autowired
 	private PizzaDAO pizzaDAO;
@@ -27,7 +61,28 @@ public class PizzaController {
 	@GetMapping("/all")
 	public String showPizzas(Model model) {
 		
-		model.addAttribute("pizzas", pizzaDAO.findAll());
+		List<Pizza> pizzas = pizzaDAO.findAll();
+		List<PizzaDTO> pizzasDTO = new ArrayList<>();
+		
+		pizzas.forEach(pizza -> {
+			List<String> ingredientsAsString = new ArrayList<>();
+			
+			pizza.getIngredients().forEach(ingr -> {
+				ingredientsAsString.add(ingr.getName()); 
+			});
+			
+			String allIngredientsAsString = String.join(", ", ingredientsAsString);
+			
+			double pizzaPrice = 0;
+			
+			for (Ingredient ingr : pizza.getIngredients()) {
+				pizzaPrice += ingr.getPrice();
+			}
+
+			pizzasDTO.add(new PizzaDTO(pizza.getId(), pizza.getName(), allIngredientsAsString, pizzaPrice));
+		});
+		
+		model.addAttribute("pizzasDTO", pizzasDTO);
 		
 		return "list-all-pizzas";
 	}
