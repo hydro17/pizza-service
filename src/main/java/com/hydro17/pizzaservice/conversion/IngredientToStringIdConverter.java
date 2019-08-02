8ce2ade2a3cfc73ebaa782995af2ce8c542a18db
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-02 09:54:35 +0200

    rename and refactor

--- src/main/java/com/hydro17/pizzaservice/conversion/IngredientToStringIdConverter.java
+++ src/main/java/com/hydro17/pizzaservice/conversion/IngredientToStringIdConverter.java
@@ -1,16 +1,12 @@
 package com.hydro17.pizzaservice.conversion;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import com.hydro17.pizzaservice.dao.IngredientDAO;
 import com.hydro17.pizzaservice.entity.Ingredient;
 
-@Component
-public class IngredientToStingIdConverter implements Converter<Ingredient, String> {
+//@Component
+public class IngredientToStringIdConverter implements Converter<Ingredient, String> {
 
 	@Override
 	public String convert(Ingredient ingredient) {
