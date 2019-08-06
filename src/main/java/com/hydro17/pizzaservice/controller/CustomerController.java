0e7334287753149f2c5c156ce6e79462cdf6a572
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-06 11:12:39 +0200

    add methods used to login

--- src/main/java/com/hydro17/pizzaservice/controller/CustomerController.java
+++ src/main/java/com/hydro17/pizzaservice/controller/CustomerController.java
@@ -1,6 +1,11 @@
 package com.hydro17.pizzaservice.controller;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +14,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hydro17.pizzaservice.dto.CustomerLoginData;
 import com.hydro17.pizzaservice.entity.Customer;
 import com.hydro17.pizzaservice.repository.CustomerRepository;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
-
+	
 	@Autowired
 	private CustomerRepository customerRepository;
 	
@@ -42,6 +48,42 @@ public class CustomerController {
 		customerRepository.save(customer);
 		
 		return "redirect:/customers/all";
+	}
+	
+	@GetMapping("/login")
+	public String showLoginForm(Model model) {
+		
+		model.addAttribute("customer", new CustomerLoginData());
+		
+		return "login-form";
+	}
+	
+	@PostMapping("/login")
+	public String verifyCustomer(@ModelAttribute CustomerLoginData customerLogingData, Model model) {
+		
+		List<Customer> customers = customerRepository.findAll();
+		
+		Customer authenticatedCustomer = getCustomerIfAuthenticated(customers, customerLogingData); 
+		
+		if (authenticatedCustomer == null) {
+			model.addAttribute("customer", customerLogingData);
+			model.addAttribute("error", "Nieprawidłoy e-mail lub hasło");
+			return "login-form";
+		}
+		
+		return "redirect:/orders/all/" + authenticatedCustomer.getId();
+	}
+	
+	private Customer getCustomerIfAuthenticated(Collection<Customer> customers, CustomerLoginData customerLogingData) {
+		
+		for (Customer customer : customers) {
+			if (customer.getEmail() == customerLogingData.getEmail() 
+				&& customer.getPassword() == customerLogingData.getPassword()) {
+				return customer;
+			}
+		}
+		
+		return null;
 	}
 	
 	@GetMapping("/update/{customerId}")
