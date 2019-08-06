097b813756827811b553f46cc5b159897501dc87
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-06 11:06:28 +0200

    add enums used in 'Order' class

---
+++ src/main/java/com/hydro17/pizzaservice/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.hydro17.pizzaservice.enums;
+
+public enum OrderStatus {
+	ORDERED, PREPARED, PICKED_UP
+}
---
+++ src/main/java/com/hydro17/pizzaservice/enums/PizzaSize.java
@@ -0,0 +1,5 @@
+package com.hydro17.pizzaservice.enums;
+
+public enum PizzaSize {
+	SMALL, MEDIUM, LARGE
+}
