e731004e3147bbca43dd84f9c858d0d02e1145d1
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-10 23:16:04 +0200

    rename from 'Order' to 'PizzaOrder'

--- src/main/java/com/hydro17/pizzaservice/repository/OrderRepository.java
+++ src/main/java/com/hydro17/pizzaservice/repository/OrderRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.hydro17.pizzaservice.entity.Order;
+import com.hydro17.pizzaservice.entity.PizzaOrder;
 
-public interface OrderRepository extends JpaRepository<Order, Integer> {
+public interface OrderRepository extends JpaRepository<PizzaOrder, Integer> {
 	
 	@Query("select o from Order o where o.customer.id = ?1")
 	List<Order> findByCustomerId(int customerId);
