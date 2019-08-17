4d214a32adf3f729061d8ab47eae406fa636c1c0
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-17 22:47:07 +0200

    upadate

--- src/main/java/com/hydro17/pizzaservice/controller/HomeController.java
+++ src/main/java/com/hydro17/pizzaservice/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.hydro17.pizzaservice.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,7 @@ public class HomeController {
 
 	@GetMapping
 	public String showMenu() {
-		return "home-page";
+		
+		return "redirect:/pizzas/list";
 	}
-	
 }
