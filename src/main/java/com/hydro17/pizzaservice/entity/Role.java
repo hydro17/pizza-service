4a83dad03df275c48ea024e50ddbea7d0391dbec
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-19 12:06:30 +0200

    change GenerationType from AUTO to IDENTITY due to the problems with Hibernate + MySQL

--- src/main/java/com/hydro17/pizzaservice/entity/Role.java
+++ src/main/java/com/hydro17/pizzaservice/entity/Role.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 public class Role {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name="name")
