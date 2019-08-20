b7a7ef2ee196e9103e197ba5b5f2b41092b50910
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-20 07:11:04 +0200

    add throwing exception in case of user == null

--- src/main/java/com/hydro17/pizzaservice/service/UserPrinciplaDetailsService.java
+++ src/main/java/com/hydro17/pizzaservice/service/UserPrinciplaDetailsService.java
@@ -20,6 +20,9 @@ public class UserPrinciplaDetailsService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		
 		User user = userRepository.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException(user.getEmail());
+		}
 		
 		return new UserPrincipal(user);
 	}
