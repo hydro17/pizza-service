c44a3e008674257773619a1597b1fed9bc9b3f73
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-21 08:46:09 +0200

    we will use the 'User' instead of the 'Customer'

--- src/main/java/com/hydro17/pizzaservice/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hydro17.pizzaservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hydro17.pizzaservice.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
