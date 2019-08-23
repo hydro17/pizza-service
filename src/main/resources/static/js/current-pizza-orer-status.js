c819090f9ee1e6a607f884f7e716a684e135a298
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-23 07:54:52 +0200

    fix typo correct formating

--- src/main/resources/static/js/current-pizza-orer-status.js
+++ src/main/resources/static/js/current-pizza-orer-status.js
@@ -1,22 +1,20 @@
-
 const table = document.querySelector("table");
 
-table.addEventListener("click", (e)=>{
-
-  // if ((e.target.parentNode.tagName !== "A") 
+table.addEventListener("click", (e) => {
+  // if ((e.target.parentNode.tagName !== "A")
   //   && (e.target.parentNode.innerHTML !== '<i class="far fa-edit" aria-hidden="true"></i>')) return;
-  
+
   if (!e.target.classList.contains("fa-edit")) return;
 
   let node = e.target;
-  
-  while (node.tagName !== 'TR') {
-	  node = node.parentNode;
+
+  while (node.tagName !== "TR") {
+    node = node.parentNode;
   }
-  
-  if (node.querySelectorAll("td")[1].innerHTML === "ORDERED") return; 
-  
-  alert("Status is not ORDERD!");
-  
+
+  if (node.querySelectorAll("td")[2].innerHTML === "ORDERED") return;
+
+  alert("Status is not ORDERED!");
+
   e.preventDefault();
 });
