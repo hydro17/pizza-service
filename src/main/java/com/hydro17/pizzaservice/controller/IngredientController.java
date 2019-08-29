c0876146cf1569e75a62cf56ac7e6a3590b9d2fd
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-08-29 07:10:12 +0200

    set stock to default value equal 1 (stocks of the ingredients are not currently used)

--- src/main/java/com/hydro17/pizzaservice/controller/IngredientController.java
+++ src/main/java/com/hydro17/pizzaservice/controller/IngredientController.java
@@ -65,7 +65,10 @@ public class IngredientController {
 	@GetMapping("/add")
 	public String showAddIngredientForm(Model model) {
 		
-		model.addAttribute("ingredient", new Ingredient());
+		Ingredient ingredient = new Ingredient();
+		ingredient.setStock(1);
+		
+		model.addAttribute("ingredient", ingredient);
 		
 		return "ingredients/add-or-update-ingredient-form";
 	}
