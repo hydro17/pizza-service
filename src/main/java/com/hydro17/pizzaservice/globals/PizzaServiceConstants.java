ef707e271fd604dd8be5e6c879f9e757a40f6378
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-09-01 14:47:26 +0200

    add field 'smallPizzaMultiplier'

--- src/main/java/com/hydro17/pizzaservice/globals/PizzaServiceConstants.java
+++ src/main/java/com/hydro17/pizzaservice/globals/PizzaServiceConstants.java
@@ -2,6 +2,7 @@ package com.hydro17.pizzaservice.globals;
 
 public class PizzaServiceConstants {
 	public static final String DATE_TIME_FORMAT = "yyyy.MM.dd HH:mm";
+	public static final double smallPizzaMultiplier = 1;
 	public static final double mediumPizzaMultiplier = 1.3;
 	public static final double bigPizzaMultiplier = 1.6;
 }
