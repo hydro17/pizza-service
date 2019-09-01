cd5a03bee30e432d2bd4136483e6041e78704be5
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-09-01 14:45:33 +0200

    add field 'priceMultiplier'

--- src/main/java/com/hydro17/pizzaservice/enums/PizzaSize.java
+++ src/main/java/com/hydro17/pizzaservice/enums/PizzaSize.java
@@ -1,15 +1,25 @@
 package com.hydro17.pizzaservice.enums;
 
+import com.hydro17.pizzaservice.globals.PizzaServiceConstants;
+
 public enum PizzaSize {
-	SMALL("mała"), MEDIUM("średnia"), LARGE("duża");
+	SMALL("mała", PizzaServiceConstants.smallPizzaMultiplier), 
+	MEDIUM("średnia", PizzaServiceConstants.mediumPizzaMultiplier), 
+	LARGE("duża", PizzaServiceConstants.bigPizzaMultiplier);
 	
 	private String polishName;
+	private double priceMultiplier;
 	
-	private PizzaSize(String polishName) {
+	private PizzaSize(String polishName, double priceMultiplier) {
 		this.polishName = polishName;
+		this.priceMultiplier = priceMultiplier;
 	}
 	
 	public String getPolishName() {
 		return polishName;
+	}
+
+	public double getPriceMultiplier() {
+		return priceMultiplier;
 	}
 }
